349f05727932347dd258ff99f8207f863e525924
jhayesnv <jhayesdev@icloud.com>
2024-10-29 14:05:42 -0700

    Finish recipe model tests

--- recipe/tests.py
+++ recipe/tests.py
@@ -29,6 +29,11 @@ class RecipeTestCase(TestCase):
             distributor=distributor
         )
 
+        item_C = im.FoodOrderItem.objects.create(
+            name='White Granulated Sugar',
+            distributor=distributor
+        )
+
         item_A.area_categories.set([category])
         item_B.area_categories.set([category])
 
@@ -44,6 +49,34 @@ class RecipeTestCase(TestCase):
 
         menu_item.recipes.set([kitchen_recipe])
 
+        simple = rm.BarRecipe.objects.create(
+            name='Simple Syrup'
+        )
+
+        simple.ingredients.set([item_C])
+
+        producer = im.Producer.objects.create(
+            name="Michter's",
+        )
+
+        region = im.Region.objects.create(
+            name='Kentucky'
+        )
+
+        whiskey = im.SpiritOrderItem.objects.create(
+            name='Sour Mash',
+            producer=producer,
+            distributor=distributor,
+            region=region
+        )
+
+        cocktail_item = rm.CocktailRecipe.objects.create(
+            name='Old Fashioned'
+        )
+
+        cocktail_item.bar_ingredients.set([simple])
+        cocktail_item.spirit_ingredients.set([whiskey])
+
     def test_menu_order_item_is_created_successfully(self):
         """ Ensure a menu item recipe is created with correct fields """
         menu_item = rm.MenuItemRecipe.objects.first()
@@ -53,3 +86,16 @@ class RecipeTestCase(TestCase):
         self.assertEqual(menu_item.recipes.first().name,
                          'Asian Vinaigrette')
         self.assertTrue(len(menu_item.recipes.all()), 2)
+
+    def test_cocktail_recipe_is_created_successfully(self):
+        """ Ensure a cocktail recipe entity is created with correct fields """
+
+        cocktail = rm.CocktailRecipe.objects.first()
+
+        self.assertEqual(type(cocktail.id), type(uuid4()))
+        self.assertEqual(cocktail.name, 'Old Fashioned')
+        self.assertEqual(cocktail.category, 'Levitate Menu')
+        self.assertEqual(cocktail.base_spirit, 'Bourbon')
+        self.assertEqual(cocktail.method, 'Shake/Strain')
+        self.assertIsNotNone(cocktail.spirit_ingredients)
+        self.assertIsNotNone(cocktail.garnish)
